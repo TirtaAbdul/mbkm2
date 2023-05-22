813d7b0d42c2928fc1305e37b9001c65e8175f82
nadilipote <107466272+nadilipote@users.noreply.github.com>
2023-05-22 14:59:39 +0700

    tambah akun TU

---
+++ tu/nilai_view.php
@@ -0,0 +1,165 @@
+<?php
+$id_formulir = $_GET['id_formulir'];
+$_SESSION['id_formulir'] = $id_formulir;
+
+?>
+
+<main class="content">
+
+    <nav aria-label="breadcrumb" class="d-none d-md-inline-block">
+        <ol class="breadcrumb breadcrumb-dark breadcrumb-transparent">
+            <li class="breadcrumb-item">
+                <a href="#">
+                </a>
+            </li>
+        </ol>
+    </nav>
+
+    <div class="row mx-8">
+        <div class="col-12 col-xl-12">
+            <div class="card card-body border-0 shadow mb-4">
+                <div class="clearfix">
+                    <a class="btn btn-danger btn-lg float-end" href="../tu/template.php?page=detail_kegiatan&&id_formulir=<?= $id_formulir ?>" role="button">Keluar (X)</a>
+                </div>
+                <h2 class="h10 mt-3 mb-5 text-center"><b>Hasil Instrumentasi Nilai Kegiatan</b></h2>
+                <div class="row mx-3">
+                    <div class="card card-body shadow">
+                        <div id="employee_table" class="table-responsive">
+                            <?php
+                            $query = "SELECT * FROM formulir WHERE id_formulir='$id_formulir'";
+                            $result = mysqli_query($conn, $query);
+
+                            if (mysqli_num_rows($result) > 0) {
+
+                                while ($data = mysqli_fetch_assoc($result)) {
+                            ?>
+                                    <table id="example" class="table table-primary" style="width:100%">
+                                        <tr>
+                                            <td width="30%"><label for="nama_mhs">Nama Lengkap Mahasiswa</label></td>
+                                            <td class="tengah">:</td>
+                                            <td><?php echo $nama ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="30%"><label for="nim">Nomor Induk Mahasiswa </label></td>
+                                            <td class="tengah">:</td>
+                                            <td><?php echo $data['nim']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="30%"><label for="prodi_asal">Program Studi </label></td>
+                                            <td class="tengah">:</td>
+                                            <td><?php echo $data['prodi_asal']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="30%"><label for="jenis_program">Jenis Program Merdeka Belajar </label></td>
+                                            <td class="tengah">:</td>
+                                            <td><?php echo $data['jenis_program']; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td width="30%"><label for="judul_program">Judul Program Merdeka Belajar</label></td>
+                                            <td class="tengah">:</td>
+                                            <td><?php echo $data['judul_program'];
+                                            }
+                                        } ?></td>
+                                        </tr>
+                                        </tr>
+                                        <?php
+                                        $query = "SELECT user.nim_nik, user.nama, assign_dospem.id_formulir, assign_dospem.nik_dospem 
+                                            FROM assign_dospem 
+                                            LEFT JOIN user 
+                                            ON assign_dospem.nik_dospem = user.nim_nik
+                                            WHERE id_formulir='$id_formulir'";
+                                        $result = mysqli_query($conn, $query);
+
+                                        if (mysqli_num_rows($result) > 0) {
+
+                                            while ($data = mysqli_fetch_assoc($result)) {
+                                        ?>
+                                                <tr>
+
+                                                    <td width="30%"><label for="jenis_program">Nama Lengkap Dosen Pembimbing </label></td>
+                                                    <td class="tengah">:</td>
+                                                    <td><?php echo $data['nama']; ?></td>
+                                                </tr>
+                                                <tr>
+                                                    <td width="30%"><label for="jenis_program">Nomor Induk Dosen Pembimbing</label></td>
+                                                    <td class="tengah">:</td>
+                                                    <td><?php echo $data['nik_dospem'];
+                                                    }
+                                                } ?></td>
+                                                </tr>
+                                    </table>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- GO -->
+                <div class="row mx-3 mt-4">
+                    <div class="card card-body shadow">
+                        <h3 class="modal-header mb-3">Rubrik Penilaian</h3>
+                        <input type="text" name="id_formulir" value="<?= $id_formulir ?>" required hidden>
+                        <div id="employee_table">
+                            <div class="table-responsive">
+                                <table class="parameter table table-striped table-bordered">
+                                    <?php
+                                    $querynilai = "SELECT * FROM nilai_kegiatan WHERE id_formulir=$id_formulir";
+                                    $resultnilai = mysqli_query($conn, $querynilai);
+
+                                    if (mysqli_num_rows($resultnilai) > 0) {
+
+                                        while ($datanilai = mysqli_fetch_assoc($resultnilai)) {
+                                    ?>
+                                            <tr>
+                                                <th rowspan="2" class="align-middle text-center">No</th>
+                                                <th rowspan="2" class="align-middle text-center">Aspek Penilaian</th>
+                                                <th colspan="4" class="align-middle text-center">Skala Penilaian</th>
+                                            </tr>
+                                            <tr>
+                                                <th>Kurang</th>
+                                                <th>Cukup</th>
+                                                <th>Baik</th>
+                                                <th>Baik Sekali</th>
+                                            </tr>
+                                            <tr>
+                                                <td>1</td>
+                                                <td style="text-align: left">Kemampuan Hardskill</td>
+                                                <?php for ($i = 4; $i > 0; $i -= 1) {
+                                                    $checked = ($datanilai['hardskill'] == $i) ? "checked" : "";
+                                                ?>
+                                                    <td><input type="radio" id="1" name="hardskill" value="<?php echo $i ?>" <?php echo $checked ?> disabled></td>
+                                                <?php } ?>
+
+                                            </tr>
+                                            <tr>
+                                                <td>2</td>
+                                                <td style="text-align: left">Kemampuan Softskill</td>
+                                                <?php for ($i = 4; $i > 0; $i -= 1) {
+                                                    $checked = ($datanilai['softskill'] == $i) ? "checked" : "";
+                                                ?>
+                                                    <td><input type="radio" id="2" name="softskill" value="<?php echo $i ?>" <?php echo $checked ?> disabled></td>
+                                                <?php } ?>
+                                            </tr>
+                                            <tr>
+                                                <td>3</td>
+                                                <td style="text-align: left">Luaran kegiatan merdeka belajar (Outcome)</td>
+                                                <?php for ($i = 4; $i > 0; $i -= 1) {
+                                                    $checked = ($datanilai['luaran'] == $i) ? "checked" : "";
+                                                ?>
+                                                    <td><input type="radio" id="3" name="luaran" value="<?php echo $i ?>" <?php echo $checked ?> disabled></td>
+                                                <?php } ?>
+                                            </tr>
+                                            <tr>
+                                                <td>4</td>
+                                                <td style="text-align: left">Capaian Pembelajaran</td>
+                                                <?php for ($i = 4; $i > 0; $i -= 1) {
+                                                    $checked = ($datanilai['capaian'] == $i) ? "checked" : "";
+                                                ?>
+                                                    <td><input type="radio" id="1" name="capaian" value="<?php echo $i ?>" <?php echo $checked ?> disabled></td>
+                                                <?php } ?>
+                                            </tr>
+                                    <?php }
+                                    } ?>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
---
+++ tu/template.php
@@ -0,0 +1,239 @@
+<?php
+session_start();
+if ($_SESSION['role'] != 'Tu') {
+    header("Location: login.php");
+}
+
+$nim = $_SESSION['nim_nik'];
+$nama = $_SESSION['nama'];
+include "../koneksi.php";
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <!-- Primary Meta Tags -->
+    <title>Merdeka Belajar Kampus Merdeka</title>
+
+    <meta name="keywords" content="bootstrap 5, bootstrap, bootstrap 5 admin dashboard, bootstrap 5 dashboard, bootstrap 5 charts, bootstrap 5 calendar, bootstrap 5 datepicker, bootstrap 5 tables, bootstrap 5 datatable, vanilla js datatable, themesberg, themesberg dashboard, themesberg admin dashboard" />
+    <link rel="canonical" href="https://themesberg.com/product/admin-dashboard/volt-premium-bootstrap-5-dashboard" />
+
+    <!-- Sweet Alert -->
+    <link type="text/css" href="../vendor/sweetalert2/dist/sweetalert2.min.css" rel="stylesheet" />
+
+    <!-- Notyf -->
+    <link type="text/css" href="../vendor/notyf/notyf.min.css" rel="stylesheet" />
+
+    <!-- Volt CSS -->
+    <link type="text/css" href="../css/volt.css" rel="stylesheet" />
+
+    <!-- Datatables CSS -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/rowreorder/1.2.8/css/rowReorder.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.9/css/responsive.dataTables.min.css">
+
+    <!-- Datatables JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="assets/npm/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/rowreorder/1.2.8/js/dataTables.rowReorder.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.9/js/dataTables.responsive.min.js"></script>
+
+    <!-- Bootstrap 5 CSS -->
+</head>
+<style>
+    .kiri {
+        width: 23%;
+    }
+
+    .tengah {
+        width: 1%;
+    }
+</style>
+
+
+<body>
+    <nav class="navbar navbar-dark navbar-theme-primary px-4 col-12 d-lg-none">
+        <a class="navbar-brand me-lg-5" href="../index.html"> <img class="navbar-brand-dark" src="../assets/img/merdeka.png" alt="Volt logo" /> <img class="navbar-brand-light" src="../assets/img/brand/dark.svg" alt="Volt logo" /> </a>
+        <div class="d-flex align-items-center">
+            <button class="navbar-toggler d-lg-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+    </nav>
+    <nav id="sidebarMenu" class="sidebar d-lg-block bg-gray-800 text-white collapse" data-simplebar>
+        <div class="sidebar-inner px-4 pt-3">
+            <div class="user-card d-flex d-md-none align-items-center justify-content-between justify-content-md-center pb-4">
+
+                <div class="collapse-close d-md-none">
+                    <a href="#sidebarMenu" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="true" aria-label="Toggle navigation">
+                        <svg class="icon icon-xs" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+            <ul class="nav flex-column pt-3 pt-md-0">
+                <li class="nav-item muted">
+                    <a href="#" class="nav-link d-flex align-items-center disabled">
+                        <span class="sidebar-icon">
+                            <img src="../assets/img/merdeka.png" height="100" width="200" />
+                        </span>
+                        <span class="mt-1 ms-1 sidebar-text"></span>
+                    </a>
+                </li>
+
+                <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
+
+                <li class="nav-item">
+                    <span class="sidebar-icon"></span>
+                    <span class="sidebar-text">Welcome to MBKM,<br><?= $nama ?><br><?= $nim ?></span>
+                    </a>
+                </li>
+
+                <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
+                <li class="nav-item  ">
+                    <a href="../tu/template.php?page=dashboard" class="nav-link">
+                        <span class="sidebar-icon">
+                            <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M12 1.586l-4 4v12.828l4-4V1.586zM3.707 3.293A1 1 0 002 4v10a1 1 0 00.293.707L6 18.414V5.586L3.707 3.293zM17.707 5.293L14 1.586v12.828l2.293 2.293A1 1 0 0018 16V6a1 1 0 00-.293-.707z" clip-rule="evenodd"></path>
+                            </svg>
+                        </span>
+                        <span class="sidebar-text">Dashboard</span>
+                    </a>
+                </li>
+
+                <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
+
+                <!-- Sub Menu -->
+                <li class="nav-item">
+                    <span class="nav-link  collapsed  d-flex justify-content-between align-items-center" data-bs-toggle="collapse" data-bs-target="#submenu-app">
+                        <span>
+                            <span class="sidebar-icon">
+                                <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" clip-rule="evenodd"></path>
+                                </svg>
+                            </span>
+                            <span class="sidebar-text">Data Pendaftar</span>
+                        </span>
+                        <span class="link-arrow">
+                            <svg class="icon icon-sm" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path>
+                            </svg>
+                        </span>
+                    </span>
+                    <div class="multi-level collapse" role="list" id="submenu-app" aria-expanded="false">
+                        <ul class="flex-column nav">
+                            <li class="nav-item">
+                                <a href="../tu/template.php?page=setuju" class="nav-link">
+                                    <span class="sidebar-icon">
+                                        <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path fill-rule="evenodd" d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z" clip-rule="evenodd"></path>
+                                        </svg>
+                                    </span>
+                                    <span class="sidebar-text">Ajuan Disetujui</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="multi-level collapse" role="list" id="submenu-app" aria-expanded="false">
+                        <ul class="flex-column nav">
+                            <li class="nav-item">
+                                <a href="../tu/template.php?page=tolak" class="nav-link">
+                                    <span class="sidebar-icon">
+                                        <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                            <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
+                                            <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm9.707 5.707a1 1 0 00-1.414-1.414L9 12.586l-1.293-1.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+                                        </svg>
+                                    </span>
+                                    <span class="sidebar-text">Ajuan Ditolak</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                <li class="nav-item  ">
+                    <a href="../tu/template.php?page=peserta_kegiatan" class="nav-link">
+                        <span class="sidebar-icon">
+                            <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" clip-rule="evenodd"></path>
+                            </svg>
+                        </span>
+                        <span class="sidebar-text">Peserta Kegiatan</span>
+                    </a>
+                </li>
+
+                <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
+
+                <li class="nav-item">
+                    <a href="../logout.php" class="nav-link">
+                        <span class="sidebar-icon">
+                            <svg class="icon icon-xs me-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M8 2a1 1 0 000 2h2a1 1 0 100-2H8z" />
+                                <path d="M3 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v6h-4.586l1.293-1.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L10.414 13H15v3a2 2 0 01-2 2H5a2 2 0 01-2-2V5zM15 11h2a1 1 0 110 2h-2v-2z" />
+                            </svg>
+                        </span>
+                        <span class="sidebar-text">Keluar</span>
+                    </a>
+                </li>
+                <li role="separator" class="dropdown-divider mt-4 mb-3 border-gray-700"></li>
+            </ul>
+        </div>
+    </nav>
+    </div>
+
+    <div id="content">
+        <?php include "../tu/config_menu.php"; // Load file config.php 
+        ?>
+    </div>
+
+    <!-- Core -->
+    <script src="../vendor/@popperjs/core/dist/umd/popper.min.js"></script>
+    <script src="../vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+
+    <!-- Vendor JS -->
+    <script src="../vendor/onscreen/dist/on-screen.umd.min.js"></script>
+
+    <!-- Slider -->
+    <script src="../vendor/nouislider/distribute/nouislider.min.js"></script>
+
+    <!-- Smooth scroll -->
+    <script src="../vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+
+    <!-- Charts -->
+    <script src="../vendor/chartist/dist/chartist.min.js"></script>
+    <script src="../vendor/chartist-plugin-tooltips/dist/chartist-plugin-tooltip.min.js"></script>
+
+    <!-- Datepicker -->
+    <script src="../vendor/vanillajs-datepicker/dist/js/datepicker.min.js"></script>
+
+    <!-- Sweet Alerts 2 -->
+    <script src="../vendor/sweetalert2/dist/sweetalert2.all.min.js"></script>
+
+    <!-- Moment JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.27.0/moment.min.js"></script>
+
+    <!-- Vanilla JS Datepicker -->
+    <script src="../vendor/vanillajs-datepicker/dist/js/datepicker.min.js"></script>
+
+    <!-- Notyf -->
+    <script src="../vendor/notyf/notyf.min.js"></script>
+
+    <!-- Simplebar -->
+    <script src="../vendor/simplebar/dist/simplebar.min.js"></script>
+
+    <!-- Github buttons -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+    <!-- Volt JS -->
+    <script src="../assets/js/volt.js"></script>
+
+    <!-- Bootstrap 5 JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
